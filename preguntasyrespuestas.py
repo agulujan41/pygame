907a29c88289ebdece24db3b09c86cb8383cca66
agulujan41proj <agulujan41proj@gmail.com>
2024-03-23 12:43:46 -0300

    v 0.0.4

--- preguntasyrespuestas.py
+++ preguntasyrespuestas.py
@@ -23,13 +23,12 @@ presentacion_juego.cambiarTexto("Bienvenidos al juego",70,NEGRO)
 continuar = textArea.TextArea(0,200,500,100,pantalla,BLANCO)
 continuar.cambiarTexto("Presiona la letra ESPACIO para continuar",30,AZUL)
 
-pregunta1 = "¿De que pais es Messi?"
-pregunta2 = "¿Cuantos habitantes tiene Bolivia?"
-pregunta3 = "¿Que es PyGame?"
+listaPreguntas = ["¿De que pais es Messi?","¿Cuantos habitantes tiene Bolivia?","¿Que es PyGame?"]
+listaRespuestas = ["Argentina","12 millones","Una libreria de Python","Narnia"]
 
-respuesta1 = "Argentina"
-respuesta2 = "12 millones"
-respuesta3 = "Una libreria de Python"
+#agregarALaLista
+listaRespuestas.append("Fornite")
+listaPreguntas.append("¿Cuantos años tiene Cristiano Ronaldo?")
 
 
 preguntas = textArea.TextArea(0,0,500,30,pantalla,BLANCO)
@@ -37,6 +36,8 @@ preguntas.cambiarTexto("Pregunta:",25,NEGRO)
 
 respuestas = textArea.TextArea(0,30,500,30,pantalla,BLANCO)
 respuestas.cambiarTexto("Respuesta:",25,NEGRO)
+
+
 def juego():
     
     
@@ -48,16 +49,12 @@ def juego():
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
-                    numero_random = random.randint(1,3)
-                    if numero_random == 1:
-                        preguntas.cambiarTexto("Pregunta:"+pregunta1,25,NEGRO)
-                    elif numero_random == 2:
-                        preguntas.cambiarTexto("Pregunta:"+pregunta2,25,NEGRO)
-                    else:
-                        preguntas.cambiarTexto("Pregunta:"+pregunta3,25,NEGRO)
+                    numero_random = random.randint(1,len(listaPreguntas))
+                    preguntas.cambiarTexto("Pregunta:"+listaPreguntas[numero_random-1],25,NEGRO)
+                    
                 if event.key == pygame.K_a:
-                   numero_random = random.randint(1,3)
-                   
+                   numero_random = random.randint(1,len(listaRespuestas))
+                   respuestas.cambiarTexto("Respuesta:"+listaRespuestas[numero_random-1],25,NEGRO)
         
         preguntas.dibujar(0,0)
         respuestas.dibujar(0,0)
